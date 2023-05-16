42080eb75dd315579b21f4be451ec68969de49e7
TURKCELL\EXT02D52981 <yakup.kalayci@consultant.turkcell.com.tr>
2023-05-16 10:29:28 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
